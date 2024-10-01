a1fbbbf90c94793e74ca8503a23cd1db4f82f8da
Martin Sarrionandia <martin@sarrionandia.co.uk>
2024-10-01 23:05:04 +0100

    Sutff

--- rancher/ingress.tf
+++ rancher/ingress.tf
@@ -1,10 +1,47 @@
 resource "kubernetes_manifest" "mojobooth-ingress" {
-  manifest = yamldecode(templatefile("${path.module}/templates/ingress.yaml",
-    {
-      namespace      = kubernetes_namespace.mojobooth.metadata.0.name,
-      app-name       = helm_release.wordpress.metadata.0.name,
-      chart-name     = helm_release.wordpress.metadata.0.chart,
-      cluster-issuer = data.terraform_remote_state.rancher-config.outputs.cluster-issuer,
-      endpoint-fqdn  = aws_route53_record.www_mojobooth.fqdn
-  }))
+  manifest = {
+    apiVersion = "networking.k8s.io/v1"
+    kind = "Ingress"
+    metadata = {
+      annotations = {
+        "cert-manager.io/cluster-issuer" = data.terraform_remote_state.rancher-config.outputs.cluster-issuer
+      }
+      labels = {
+        app = helm_release.wordpress.metadata.0.chart
+      }
+      name = helm_release.wordpress.metadata.0.name
+      namespace = kubernetes_namespace.mojobooth.metadata.0.name
+    }
+    spec = {
+      rules = [
+        {
+          host = aws_route53_record.www_mojobooth.fqdn
+          http = {
+            paths = [
+              {
+                backend = {
+                  service = {
+                    name = "${helm_release.wordpress.metadata.0.name}-${helm_release.wordpress.metadata.0.chart}"
+                    port = {
+                      number = 80
+                    }
+                  }
+                }
+                path = "/"
+                pathType = "Prefix"
+              },
+            ]
+          }
+        },
+      ]
+      tls = [
+        {
+          hosts = [
+            aws_route53_record.www_mojobooth.fqdn,
+          ]
+          secretName = data.terraform_remote_state.rancher-config.outputs.cluster-issuer
+        },
+      ]
+    }
+  }
 }
